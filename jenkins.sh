6b1dc372f528ca9b9b692589db7bb93c69310534
Adrinems <adrianasorianom@gmail.com>
2014-11-03 08:40:05 -0600

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+pip install -r requirements.txt
+autopep8 -ir .
+flake8 --max-complexity=3 --exclude=*.txt,*.md --max-line-length=200 .
+
+lettuce AcceptanceTest
+cd UnitTest
+python TestCase.py -v
+coverage run --branch TestCase.py
+coverage report -m
+coverage html --title="Coverage about Figures"
